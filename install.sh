da29f493626a8936494a6ad5eac2430e94940d57
Dave York <d4v3y0rk@users.noreply.github.com>
2016-11-03 20:48:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,8 +18,8 @@ cp import_users.sh /opt/import_users.sh
 chmod +x /opt/import_users.sh
 
 if [ -f /etc/lsb-release ]; then
-  echo "AuthorizedKeysCommand \"/opt/authorized_keys_command.sh\"" >> /etc/ssh/sshd_config
-  echo "AuthorizedKeysCommandUser nobody" >> /etc/ssh/sshd_config
+  echo "#AuthorizedKeysCommand \"/opt/authorized_keys_command.sh\"" >> /etc/ssh/sshd_config
+  echo "#AuthorizedKeysCommandUser nobody" >> /etc/ssh/sshd_config
 else
   sed -i 's:#AuthorizedKeysCommand none:AuthorizedKeysCommand /opt/authorized_keys_command.sh:g' /etc/ssh/sshd_config
   sed -i 's:#AuthorizedKeysCommandUser nobody:AuthorizedKeysCommandUser nobody:g' /etc/ssh/sshd_config
