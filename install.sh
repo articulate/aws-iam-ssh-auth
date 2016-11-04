79c5222d0161e5fdfdd66c2b0a3dafd0e2645550
Dave York <d4v3y0rk@users.noreply.github.com>
2016-11-03 20:23:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,11 +17,13 @@ chmod +x /opt/authorized_keys_command.sh
 cp import_users.sh /opt/import_users.sh
 chmod +x /opt/import_users.sh
 
-sed -i 's:#AuthorizedKeysCommand none:AuthorizedKeysCommand /opt/authorized_keys_command.sh:g' /etc/ssh/sshd_config
-sed -i 's:#AuthorizedKeysCommandUser nobody:AuthorizedKeysCommandUser nobody:g' /etc/ssh/sshd_config
-
-python -mplatform | grep -qi Ubuntu && echo "AuthorizedKeysCommand /opt/authorized_keys_command.sh" >> /etc/ssh/sshd_config 
-python -mplatform | grep -qi Ubuntu && echo "AuthorizedKeysCommandUser nobody" >> /etc/ssh/sshd_config
+if [ -f /etc/lsb-release ]; then
+  echo "AuthorizedKeysCommand \"/opt/authorized_keys_command.sh\"" >> /etc/ssh/sshd_config
+  echo "AuthorizedKeysCommandUser nobody" >> /etc/ssh/sshd_config
+else
+  sed -i 's:#AuthorizedKeysCommand none:AuthorizedKeysCommand /opt/authorized_keys_command.sh:g' /etc/ssh/sshd_config
+  sed -i 's:#AuthorizedKeysCommandUser nobody:AuthorizedKeysCommandUser nobody:g' /etc/ssh/sshd_config
+fi
 
 echo "*/10 * * * * root /opt/import_users.sh" > /etc/cron.d/import_users
 chmod 0644 /etc/cron.d/import_users
