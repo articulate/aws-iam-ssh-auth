3eef051398a51cf2bbae7b749f420eacd5463074
Adam Ochonicki <fromonesrc@users.noreply.github.com>
2017-04-03 15:56:42 -0400

    Remove logging when user exists
    So we don't fill up logs with garbage

--- import_users.sh
+++ import_users.sh
@@ -15,11 +15,7 @@ export AWS_SECRET_ACCESS_KEY="${sts[1]}"
 export AWS_SESSION_TOKEN="${sts[2]}"
 
 /usr/local/bin/aws iam list-users --query "Users[].[UserName]" --output text | while read User; do
-  if id -u "$User" >/dev/null 2>&1; then
-    echo "$User exists"
-  else
-    python -mplatform | grep -qi Ubuntu && sudo /usr/sbin/adduser --gecos "" --disabled-password "$User" || /usr/sbin/adduser --comment "IAM" "$User"
-    echo "$User ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$User"
-    chmod 0440 /etc/sudoers.d/$User
-  fi
+  python -mplatform | grep -qi Ubuntu && sudo /usr/sbin/adduser --gecos "" --disabled-password "$User" || /usr/sbin/adduser --comment "IAM" "$User"
+  echo "$User ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$User"
+  chmod 0440 /etc/sudoers.d/$User
 done
