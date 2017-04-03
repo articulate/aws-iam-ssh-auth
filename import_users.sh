f155e52211db98f40fa0424b0eb097bee2bd8508
Adam Ochonicki <fromonesrc@users.noreply.github.com>
2017-04-03 14:24:25 -0400

    Hardcode AWS bin path

--- import_users.sh
+++ import_users.sh
@@ -3,7 +3,7 @@
 session_name="`hostname`-`date +%Y%m%d`"
 
 sts=( $(
-    aws sts assume-role \
+    /usr/local/bin/aws sts assume-role \
     --role-arn "arn:aws:iam::689543204258:role/allow-ssh" \
     --role-session-name "$session_name" \
     --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
@@ -14,7 +14,7 @@ export AWS_ACCESS_KEY_ID="${sts[0]}"
 export AWS_SECRET_ACCESS_KEY="${sts[1]}"
 export AWS_SESSION_TOKEN="${sts[2]}"
 
-aws iam list-users --query "Users[].[UserName]" --output text | while read User; do
+/usr/local/bin/aws iam list-users --query "Users[].[UserName]" --output text | while read User; do
   if id -u "$User" >/dev/null 2>&1; then
     echo "$User exists"
   else
