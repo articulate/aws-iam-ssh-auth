8835df26a41f48192189fb1925e9ba668debfa40
Brian Stolz <brian@tecnobrat.com>
2017-05-19 07:56:41 -0700

    Remove hardcoded paths.

--- import_users.sh
+++ import_users.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export PATH=/usr/local/bin:/usr/bin:$PATH
+
 session_name="`hostname`-`date +%Y%m%d`"
 
 sts=( $(
-    /usr/local/bin/aws sts assume-role \
+    aws sts assume-role \
     --role-arn "arn:aws:iam::689543204258:role/allow-ssh" \
     --role-session-name "$session_name" \
     --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
@@ -14,7 +16,7 @@ export AWS_ACCESS_KEY_ID="${sts[0]}"
 export AWS_SECRET_ACCESS_KEY="${sts[1]}"
 export AWS_SESSION_TOKEN="${sts[2]}"
 
-/usr/local/bin/aws iam list-users --query "Users[].[UserName]" --output text | while read User; do
+aws iam list-users --query "Users[].[UserName]" --output text | while read User; do
   python -mplatform | grep -qi Ubuntu && sudo /usr/sbin/adduser --gecos "" --disabled-password "$User" || /usr/sbin/adduser --comment "IAM" "$User"
   echo "$User ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$User"
   chmod 0440 /etc/sudoers.d/$User
