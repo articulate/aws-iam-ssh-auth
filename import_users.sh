7c33ca3d1642e4d92349c869938eda46a468279d
Adam Ochonicki <ocxo@users.noreply.github.com>
2019-11-01 10:29:47 -0400

    Scope group account creation

--- import_users.sh
+++ import_users.sh
@@ -16,7 +16,21 @@ export AWS_ACCESS_KEY_ID="${sts[0]}"
 export AWS_SECRET_ACCESS_KEY="${sts[1]}"
 export AWS_SESSION_TOKEN="${sts[2]}"
 
-aws iam list-users --query "Users[].[UserName]" --output text | while read User; do
+aws iam get-group --group-name ops --query "Users[].[UserName]" --output text | while read User; do
+  python -mplatform | grep -qi Ubuntu && sudo /usr/sbin/adduser --gecos "" --disabled-password "$User" || /usr/sbin/adduser --comment "IAM" "$User"
+  echo "$User ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$User"
+  chmod 0440 /etc/sudoers.d/$User
+  chown -R $User:$User /home/$User
+done
+
+aws iam get-group --group-name devs --query "Users[].[UserName]" --output text | while read User; do
+  python -mplatform | grep -qi Ubuntu && sudo /usr/sbin/adduser --gecos "" --disabled-password "$User" || /usr/sbin/adduser --comment "IAM" "$User"
+  echo "$User ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$User"
+  chmod 0440 /etc/sudoers.d/$User
+  chown -R $User:$User /home/$User
+done
+
+aws iam get-group --group-name it --query "Users[].[UserName]" --output text | while read User; do
   python -mplatform | grep -qi Ubuntu && sudo /usr/sbin/adduser --gecos "" --disabled-password "$User" || /usr/sbin/adduser --comment "IAM" "$User"
   echo "$User ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$User"
   chmod 0440 /etc/sudoers.d/$User
